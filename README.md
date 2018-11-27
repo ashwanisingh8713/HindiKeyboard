cd2b30be58645ef4a4dcb995c4e5af9f8c552619
Ashwani <ashwani@NITL-003426.local>
2018-11-27 16:27:21 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# HindiKeyboard
---
+++ app/src/main/java/com/igeniusdev/hindidictionary/utility/Constants.java
@@ -0,0 +1,12 @@
+package com.igeniusdev.hindidictionary.utility;
+
+import android.os.Environment;
+
+import com.igeniusdev.hindidictionary.R;
+import com.igeniusdev.hindidictionary.fragment.SearchFragment;
+
+public class Constants {
+	public static final String ENG_FILIPIONO = "eng_filipino_flag";
+	public static final String LANGUAGE_1 = "English-Hindi";
+	public static final String LANGUAGE_2 = "Hindi-English";
+}
